8938ff3867502f79ad8b96fd4a322e2c81a04a45
Jinesh-a-r <jinesh.ar@OPN-LP-037.WWO.local>
2021-10-13 12:27:42 +0530

    global variables added

--- app/Services/ViewDetails.php
+++ app/Services/ViewDetails.php
@@ -9,7 +9,7 @@ class ViewDetails
     public function getDetails($uuid)
     {
         $ch = curl_init();
-        $url = 
+        $url = config("global.base_url").config("global.category_endpoint").$uuid;
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $response = json_decode(curl_exec($ch), true);
