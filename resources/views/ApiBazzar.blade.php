6ed61348b275b2d0c283cf0898e9c6401dc69bb6
Adhika K <adhika.k@OPN-MC-161.local>
2021-10-14 14:53:01 +0530

    data available check

---
+++ resources/views/ApiBazzar.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/details.css')}}">
+    <title>ApiBazzar</title>
+</head>
+<body>
+    
+     
+      <div class="conten">
+
+      @if(!empty($message))
+      <h1>{{$message}}</h1>
+      @else
+      <div class="Header">
+
+<div class ="left">
+   
+  <h2>{{$item['name']}}</h2>
+  <h4>By {{$item['provider_name']}}</h3>
+</div>
+<div class="right">
+   <img src="" alt="ApiLogos">
+</div>           
+</div>
+<div class="Body">
+<h2>About</h2>
+<?php
+ echo htmlspecialchars_decode($item['about_us']);
+?>
+</div>
+@endif  
+
+      </div>
+  
+  
+</body>
+</html>
